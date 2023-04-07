08121d58dcb38f46686fbd17449972e46543bea7
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2023-04-06 23:47:56 -0400

    added missing column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_032338) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_034633) do
   create_table "pokemons", force: :cascade do |t|
+    t.string "name", null: false
     t.string "type_1", null: false
     t.string "type_2"
     t.integer "total", null: false
