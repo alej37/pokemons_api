55bbba3828c625cf23657f24d9018e0ae9403258
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2023-04-07 21:22:57 -0400

    added factory bot pokemons for testing added test for model

---
+++ spec/factories/pokemons.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :pokemon do
+    name {"Pikachu"}
+    type_1 {"Electric"}
+    type_2 { }
+    total { 320 }
+    hp { 35 }
+    attack { 55 }
+    defense { 40 }
+    sp_atk { 50 }
+    sp_def { 50 }
+    speed { 90 }
+    generation { 1 }
+    legendary { true }
+  end
+end
